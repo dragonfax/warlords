20d06c355fe1161607898b98b489eb689602ab66
Jason Stillwell <jstillwell@wheels.co>
2020-01-04 23:39:06 -0800

    .

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"github.com/veandco/go-sdl2/gfx"
 	"github.com/veandco/go-sdl2/sdl"
+	"math"
 )
 
-const SCREEN_WIDTH = 800
-const SCREEN_HEIGHT = 600
-const MAX_AXIS = 32767
-const MIN_AXIS = -MAX_AXIS
-const AXIS_RANGE = MAX_AXIS * 2
-const AXIS_WIDTH_STEP = float32(SCREEN_WIDTH) / float32(AXIS_RANGE)
-const AXIS_HEIGHT_STEP = float32(SCREEN_HEIGHT) / float32(AXIS_RANGE)
+const WINDOW_WIDTH = 800
+const WINDOW_HEIGHT = 600
+const SCREEN_WIDTH = WINDOW_WIDTH
+const SCREEN_HEIGHT = WINDOW_HEIGHT
+
+const MAX_AXIS = math.MaxInt16
+const MIN_AXIS = math.MinInt16
+const AXIS_RANGE = MAX_AXIS - MIN_AXIS
+const AXIS_SENSITIVITY = 2
 
 var grey sdl.Color = sdl.Color{0x80, 0x80, 0x80, 0xFF}
 var red sdl.Color = sdl.Color{0xff, 0x00, 0x00, 0xFF}
@@ -23,16 +26,20 @@ func main() {
 	}
 	defer sdl.Quit()
 
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, sdl.WINDOW_SHOWN)
+	// sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1")
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
 	defer window.Destroy()
+	// sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1")
 
 	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
 		panic(err)
 	}
+	// renderer.SetScale(0.5, 0.5)
+	renderer.SetLogicalSize(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 	joystick := sdl.JoystickOpen(0)
 	if joystick == nil {
@@ -47,13 +54,20 @@ func main() {
 		renderer.SetDrawColor(0, 0, 0, 0xFF)
 		renderer.Clear()
 
-		gfx.CircleColor(renderer, 200, 200, 50, grey)
-		var p1loc float32 = (float32(p1) + MAX_AXIS) / (AXIS_RANGE / 360)
-		gfx.ArcColor(renderer, 200, 200, 70, int32(0+p1loc), int32(90+p1loc), red)
+		var baseRadius int32 = SCREEN_WIDTH / 16
+		var shieldRadius int32 = SCREEN_HEIGHT / 10
 
-		gfx.CircleColor(renderer, 600, 400, 50, grey)
-		var p2loc float32 = (float32(p2) + MAX_AXIS) / (AXIS_RANGE / 360)
-		gfx.ArcColor(renderer, 600, 400, 70, int32(0+p2loc), int32(90+p2loc), blue)
+		var p1centerX int32 = SCREEN_WIDTH * 0.25
+		var p1centerY int32 = SCREEN_HEIGHT / 3
+		var p1loc float32 = ((float32(p1) + MAX_AXIS) / (AXIS_RANGE / 360) * AXIS_SENSITIVITY)
+		gfx.FilledPieColor(renderer, p1centerX, p1centerY, shieldRadius, int32(0+p1loc), int32(90+p1loc), red)
+		gfx.FilledCircleColor(renderer, p1centerX, p1centerY, baseRadius, grey)
+
+		var p2centerX int32 = SCREEN_WIDTH * 0.75
+		var p2centerY int32 = SCREEN_HEIGHT * 2 / 3
+		var p2loc float32 = ((float32(p2) + MAX_AXIS) / (AXIS_RANGE / 360) * AXIS_SENSITIVITY)
+		gfx.FilledPieColor(renderer, p2centerX, p2centerY, shieldRadius, int32(0+p2loc), int32(90+p2loc), blue)
+		gfx.FilledCircleColor(renderer, p2centerX, p2centerY, baseRadius, grey)
 
 		renderer.Present()
 
